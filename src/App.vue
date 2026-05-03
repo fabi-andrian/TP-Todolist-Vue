7231a5eaede82f2b0da95d721159e6be529ac22d
fabi-andrian <andrianirinafabrice2024@gmail.com>
2026-05-03 21:54:42 +0300

    1st step done!

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
-
+  <fieldset role="group">
+    <input 
+    type="text"
+    placeholder="Tâche à effectuer">
+    <button>Ajouter</button>
+  </fieldset>
+  <div v-if="ToDo.length == 0">Vous n'avez pas de tâches à faire :(</div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+const ToDo = ref([])
 </script>
